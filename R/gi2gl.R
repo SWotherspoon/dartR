80be8015c5ab6f4c56ecb6184591f2958ee1ccaa
Simon Wotherspoon <Simon.Wotherspoon@utas.edu.au>
2019-01-05 17:28:16 +1100

    Rewrite of genlight/genind interchange
    The gl2gi converter used dataframes as an intermediary and the bulk of the conversion was performed with df2genind.  This version constructs the genind object directly assuming ploidy=2, and is substantially faster.
    
    The gi2gl converter was iteratively calculating a cumulative sum, and has been simplified.

--- R/gi2gl.R
+++ R/gi2gl.R
@@ -1,22 +1,16 @@
-#' Converts a genind object to genlight object
-#' 
-#' @param gi -- a genind object
-#' @param parallel -- switch to deactivate parallel version. Default set to TRUE. Only for testing purpose. 
-#' @return A genlight object, with all slots filled.
-#' @export
-#' @author Bernd Gruber (bugs? Post to \url{https://groups.google.com/d/forum/dartr})
-#' @details Be aware due to ambiguity which one is the reference allele a combination of gi2gl(gl2gi(gl)) does not return an identical object (but in terms of analysis this conversions are equivalent)
-
-
-gi2gl <- function(gi, parallel=TRUE)
-{
-  locna <- gi@loc.n.all
-  ccc<-1
-  for (i in 2:length(locna)) 
-  {
-    if (locna[i-1]==1)  ccc[i] <- ccc[i-1]+1 else ccc[i]<- ccc[i-1]+2
-  }
-  gl <-new("genlight", gi@tab[,ccc], pop = pop(gi), other=gi@other, ploidy=2, loc.names=locNames(gi), ind.names=indNames(gi), parallel=parallel)
-  return(gl)
+##' Converts a genind object to genlight object.
+##'
+##' Note that \code{gl2gi(gi2gl(x))} may not return the original
+##' object due to the ambiguity in choice of the reference alleles.
+##' @title Genind to genlight conversion
+##' @param gi a genind object
+##' @param parallel Switch passed to genlight constructor.
+##' @return A genlight object, with all slots filled.
+##' @seealso \code{\link{gi2gl}}, \code{\link[adegenet]{genlight}}
+##' @author Bernd Gruber (bugs? Post to \url{https://groups.google.com/d/forum/dartr})
+##' @export
+gi2gl <- function(gi, parallel=TRUE) {
+  locna <- unname(gi@loc.n.all)
+  new("genlight", gi@tab[,cumsum(c(1,locna[-length(locna)]))], pop = pop(gi), other=gi@other, ploidy=2, loc.names=locNames(gi), ind.names=indNames(gi), parallel=parallel)
 }
 
--- R/gl2gi.r
+++ R/gl2gi.r
@@ -1,61 +1,30 @@
-#' Converts a genlight object to genind object
-#' 
-#' @param x -- a genlight object
-#' @param probar -- if TRUE, a progress bar will be displayed for long loops [default = TRUE]
-#' @param v -- level of verbosity. v=0 is silent, v=1 returns more detailed output during conversion.
-#' @return A genind object, with all slots filled.
-#' @export
-#' @author Bernd Gruber (bugs? Post to \url{https://groups.google.com/d/forum/dartr})
-#' @details this function uses a faster version of df2genind (from the adgegenet package)
-
-gl2gi <- function(gl, probar=TRUE, v=2) {
-
-if (v==1) {
-  cat("Start conversion....\n")
-  ptm <- proc.time()[3]
-  cat("Please note conversion of bigger data sets will take some time!\n" )
-  cat("Once finished, we recommend to save the object using >save(object, file=\"object.rdata\")\n")
-}
-#convert to genind....
-x <- as.matrix(x[,])
-
-if (probar) {pb <- txtProgressBar(min=0, max=1, style=3, initial=NA)}
-
-  if (is.null(gl@loc.all))  {
-    gl@loc.all <- rep("A/T", nLoc(gl))
-    gl@loc.all[1]<- "C/G"
-  }
-
-
-homs1 <- paste(substr(gl@loc.all,1,1),"/",substr(gl@loc.all,1,1), sep = "")
-hets <-  gl@loc.all
-homs2 <- paste(substr(gl@loc.all,3,3),"/",substr(gl@loc.all,3,3), sep = "")
-xx <- matrix(NA, ncol=ncol(x), nrow=nrow(x))
-for (i in 1:nrow(x))
-  {
-  for (ii in 1:ncol(x))
-    {
-
-    inp <- x[i,ii]
-    if (!is.na(inp))
-      {
-      
-      if (inp==0) xx[i,ii] <- homs1[ii] else if (inp==1) xx[i,ii] <- hets[ii] else if (inp==2) xx[i,ii] <- homs2[ii]
-      } else xx[i,ii]="-/-"
-    }
-  if (probar) {setTxtProgressBar(pb, i/nrow(x))}
-
-if (v==1) {
-  cat("\nMatrix converted.. Prepare genind object...\n")
-}
-  if (probar) {close(pb)}
-
-gen<-df2genind(xx[,], sep="/", ncode=1, ind.names=gl@ind.names, pop = gl@pop, ploidy=2,  NA.char = "-")#, probar=probar)
-gen@other <- gl@other
-locNames(gen)<- locNames(gl)
-
-if (v==1)cat(paste("Finished! Took", round(proc.time()[3]-ptm),"seconds.\n") )
-gen
+##' Convert a genlight object to a genind object.
+##'
+##' Note that \code{gl2gi(gi2gl(x))} may not return the original
+##' object due to ordering of major alleles.
+##'
+##' @title Genlight to genind conversion
+##' @param gl a genlight object
+##' @return A genind object, with all slots filled.
+##' @seealso \code{\link{gi2gl}}, \code{\link[adegenet]{df2genind}}
+##' @author Bernd Gruber (bugs? Post to \url{https://groups.google.com/d/forum/dartr})
+##' @export
+gl2gi <- function(gl) {
+  cl <- match.call()
+  x <- as.matrix(gl)
+  cnms <- paste(rep(colnames(x),each=2),
+                ## If no loci allele names, make them up
+                if(!is.null(gl@loc.all)) unlist(strsplit(gl@loc.all,"/")) else c("A","T","C","G"),
+                sep=".")
+  ## Create frequency table (assuming ploidy=2)
+  x <- x[,rep(seq_len(ncol(x)),each=2)]
+  x[,seq.int(1,ncol(x),2)] <- 2-x[,seq.int(1,ncol(x),2)]
+  ## Drop all zero cols (df2genind also drops all zero rows, but might wreck @other)
+  keepc <- colSums(x,na.rm=TRUE)>0
+  x <- x[,keepc]
+  gi <- genind(tab=x,pop=pop(gl),prevcall=cl,ploidy=2,type="codom")
+  gi@other <- gl@other
+  gi
 }
 
 
